fe46533080efc54ddfaa7af98280d2761501b779
ManhTuanST04 <35447068+ManhTuanST04@users.noreply.github.com>
2018-03-19 15:29:43 +0700

    Connect

---
+++ QL_HocSinh_GiaoVien_THPT/QL_HocSinh_GiaoVien_THPT/DTO/Connect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace QL_HocSinh_GiaoVien_THPT.DTO
+{
+    
+   
+    public static class Connect
+    {
+        public static SqlConnection myconnect = new SqlConnection(DTO.ConnectionString.stringConnect);
+
+        public static void openConnect()
+        {
+            myconnect.Open();
+        }
+
+        public static void closeConnect()
+        {
+            myconnect.Close();
+        }
+
+    }
+
+
+
+}
--- QL_HocSinh_GiaoVien_THPT/QL_HocSinh_GiaoVien_THPT/GUI/ucGiaoVien.Designer.cs
+++ QL_HocSinh_GiaoVien_THPT/QL_HocSinh_GiaoVien_THPT/GUI/ucGiaoVien.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,10 +42,30 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Quản lý giáo viên (Chinh+ Hằng)";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(568, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hằng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "amnvdjdj";
+            // 
             // ucGiaoVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "ucGiaoVien";
             this.Size = new System.Drawing.Size(1025, 513);
@@ -55,5 +77,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
